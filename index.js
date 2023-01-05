3066aa1cf74eedbceb1a783387cf13d3c12e557e
jlopez <jocampo@onikom.mx>
2023-01-05 14:09:14 -0600

    Added the functionality of scanning the files from a folder and execute the pdf parser on each of those files

--- index.js
+++ index.js
@@ -1,42 +1,56 @@
 const PdfExtractor = require("pdf-extractor").PdfExtractor;
-let { processPDFDataIntoBD } = require("./utils/mysqlUtil");
-let convertPDFToObject = require("./utils/pdfToObject.js");
+const { processPDFDataIntoBD, pool} = require("./utils/mysqlUtil");
+const convertPDFToObject = require("./utils/pdfToObject.js");
+const fs = require('fs');
+const path = require('path');
 
-let outputDir = "./",
-  pdfExtractor = new PdfExtractor(outputDir, {
-    viewportScale: (width, height) => {
-      //dynamic zoom based on rendering a page to a fixed page size
-      if (width > height) {
-        //landscape: 1100px wide
-        return 1100 / width;
-      }
-      //portrait: 800px wide
-      return 800 / width;
-    },
-    pageRange: [1, 5],
-  });
+const directoryPath = path.join('C:', 'Rotoplas', 'Argumentalia', 'PDFs de Audiencias');
 
-pdfExtractor
-  .parse("test.pdf")
-  .then(function () {
-    console.log("# End of Document");
-    const fs = require('fs');
-    const path = require('path');
-    const body = fs
-      .readFileSync(path.resolve(__dirname, "./", "text-1.html"))
-      .toString();
-    const x = body.replace(/(<([^>]+)>)/gi, "\n");
-    const pdfElements = x.split("\n");
-    const filteredElements = pdfElements.filter((element) => {
-      return element !== "" && element !== " ";
+fs.readdir(directoryPath, (err, files) => {
+
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    }
+
+    let outputDir = "./",
+        pdfExtractor = new PdfExtractor(
+            outputDir, { viewportScale: (width, height) => {
+                    //dynamic zoom based on rendering a page to a fixed page size
+                    if (width > height) {
+                        //landscape: 1100px wide
+                        return 1100 / width;
+                    }
+                    //portrait: 800px wide
+                    return 800 / width;
+                },
+                pageRange: [1, 5],
+            });
+
+    files.forEach(file => {
+        let filePath = path.join(directoryPath, file);
+        console.log(filePath);
+        pdfExtractor
+            .parse(filePath)
+            .then(() => {
+                console.log("# End of Document");
+                const body = fs.readFileSync(path.resolve(__dirname, "./", "text-1.html")).toString();
+                const x = body.replace(/(<([^>]+)>)/gi, "\n");
+                const pdfElements = x.split("\n");
+                const filteredElements = pdfElements.filter((element) => {
+                    return element !== "" && element !== " ";
+                });
+                const pdfObject = convertPDFToObject(filteredElements);
+                console.log(`Componentes del PDF: ${JSON.stringify(pdfObject)}`);
+                processPDFDataIntoBD(pdfObject)
+                    .then((res) => {
+                        console.log(res);
+                    })
+                    .catch(console.error);
+            })
+            .catch(function (err) {
+                console.error("Error: " + err);
+            });
     });
-    const pdfObject = convertPDFToObject(filteredElements);
-    console.log(`Componentes del PDF: ${JSON.stringify(pdfObject)}`);
-    processPDFDataIntoBD(pdfObject)
-        .then(console.log)
-        .catch(console.warn);
-  })
-  .catch(function (err) {
-    console.error("Error: " + err);
-  });
+});
+
 

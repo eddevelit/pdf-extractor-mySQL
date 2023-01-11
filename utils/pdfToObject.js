14190712e5239cebb54397390c2918e55839c143
Eduardo Ocampo <edd.develit@gmail.com>
2023-01-11 14:07:41 -0600

    Modified the getParticipantsFromHTML function to return an array with the participants

--- utils/pdfToObject.js
+++ utils/pdfToObject.js
@@ -1,63 +1,72 @@
 // const fs = require("fs");
 
-const getParticipantsFromHTML = (body) => {
+const getParticipantsFromHTML = async (body) => {
+    try {
+        const parteDemandadaIndex = body.indexOf('Parte Demandada');
+        const centroDeJusticiaIndex = body.lastIndexOf('Centro de justicia</');
+        let personalSection = body.substring(parteDemandadaIndex + 22, centroDeJusticiaIndex);
+        // console.log(personalSection.bgCyan);
 
-    const parteDemandadaIndex = body.indexOf('Parte Demandada');
-    const centroDeJusticiaIndex = body.lastIndexOf('Centro de justicia</');
-    let personalSection = body.substring(parteDemandadaIndex + 22, centroDeJusticiaIndex);
-    // console.log(personalSection.bgCyan);
+        // Removing br tags
+        personalSection = personalSection.replace(/(<br role="presentation">)/gi, "\n");
+        // console.log(`Removing br tags`.blue);
+        // console.log(personalSection.bgCyan);
 
-    // Removing br tags
-    personalSection = personalSection.replace(/(<br role="presentation">)/gi, "\n");
-    // console.log(`Removing br tags`.blue);
-    // console.log(personalSection.bgCyan);
+        // Removing empty span tags
+        while (personalSection.includes('> </span>')) {
 
-    // console.log(`emptySpan?: ${personalSection.includes('> </span>')}`.yellow);
+            let emptySpanSection = personalSection.substring(0, personalSection.indexOf('> </span>') + 9);
+            // fs.writeFileSync(`emptySection.txt`, emptySpanSection);
+            console.log(`emptySpanSection`.blue);
+            console.log(emptySpanSection.bgGreen);
 
-    // Removing empty span tags
-    while (personalSection.includes('> </span>')) {
-
-        let emptySpanSection = personalSection.substring(0, personalSection.indexOf('> </span>') + 9);
-        // fs.writeFileSync(`emptySection.txt`, emptySpanSection);
-        console.log(`emptySpanSection`.blue);
-        console.log(emptySpanSection.bgGreen);
-
-        personalSection = personalSection.slice(0, emptySpanSection.lastIndexOf('<span')) + personalSection.slice(emptySpanSection.length, personalSection.length);
-        console.log(`personalSection`.blue);
-        console.log(personalSection.bgBlue);
-    }
-
-//     Ordening personal
-    personalSection = personalSection.trim();
-
-    // fs.writeFileSync(`personalSection.txt`, personalSection);
-
-    let personalArray = personalSection.split("</span>");
-    // personalArray.pop();
-    // console.table(personalArray);
-
-    for (let i = 0; i < personalArray.length - 1; i++) {
-
-        let personal = personalArray[i].substring(personalArray[i].indexOf('>') + 1, personalArray[i].length);
-
-        while ((parseFloat(personalArray[i + 1].substring(personalArray[i + 1].indexOf('left:') + 6, personalArray[i + 1].indexOf('px'))) -
-            parseFloat(personalArray[i].substring(personalArray[i].indexOf('left:') + 6, personalArray[i].indexOf('px')))
-            < 130)) {
-
-            let nextPersonal = personalArray[i + 1].substring(personalArray[i + 1].indexOf('>') + 1, personalArray[i + 1].length);
-            // console.log(`currentPersonal: ${currentPersonal}`.blue);
-            // console.log(`nextPersonal: ${nextPersonal}`.blue);
-
-            personal = personal + " " + nextPersonal;
-            i++;
-            if (i === personalArray.length - 1) {
-                break;
-            }
+            personalSection = personalSection.slice(0, emptySpanSection.lastIndexOf('<span')) + personalSection.slice(emptySpanSection.length, personalSection.length);
+            console.log(`personalSection`.blue);
+            console.log(personalSection.bgBlue);
         }
 
-        console.log(`personal: ${personal}`.bgGreen);
+//     Ordening personal
+        personalSection = personalSection.trim();
 
+        // fs.writeFileSync(`personalSection.txt`, personalSection);
+
+        let personalArray = personalSection.split("</span>");
+        // personalArray.pop();
+        // console.table(personalArray);
+
+        let completedPersonal = [];
+
+        for (let i = 0; i < personalArray.length - 1; i++) {
+
+            let personal = personalArray[i].substring(personalArray[i].indexOf('>') + 1, personalArray[i].length);
+
+            while ((parseFloat(personalArray[i + 1].substring(personalArray[i + 1].indexOf('left:') + 6, personalArray[i + 1].indexOf('px'))) -
+                parseFloat(personalArray[i].substring(personalArray[i].indexOf('left:') + 6, personalArray[i].indexOf('px')))
+                < 130)) {
+
+                let nextPersonal = personalArray[i + 1].substring(personalArray[i + 1].indexOf('>') + 1, personalArray[i + 1].length);
+                // console.log(`currentPersonal: ${currentPersonal}`.blue);
+                // console.log(`nextPersonal: ${nextPersonal}`.blue);
+
+                personal = personal + " " + nextPersonal;
+
+                i++;
+                if (i === personalArray.length - 1) {
+                    break;
+                }
+            }
+
+            console.log(`personal: ${personal}`.bgGreen);
+            completedPersonal.push(personal);
+
+        }
+
+        console.log(`completedPersonal: ${completedPersonal}`.bgBlue);
+        return completedPersonal;
+    } catch (e) {
+        throw new Error(e);
     }
+
 };
 
 const convertPDFToObject = (pdfElementsArray) => {

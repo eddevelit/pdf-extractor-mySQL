468bf06e3b590cdfac5554e6e39b4bf96798f6b1
Eduardo Ocampo <edd.develit@gmail.com>
2023-01-11 15:42:41 -0600

    Added colors to the logs :) And added a condition to prevent the addition of the testigo field

--- utils/mysqlUtil.js
+++ utils/mysqlUtil.js
@@ -32,9 +32,9 @@ pool.query = util.promisify(pool.query)
 
 const obtenerId = async (campo, valor, tabla) => {
     try {
-        console.log(`buscando id de tabla: ${tabla} con los datos => campo: ${campo}, valor: ${valor} `)
+        console.log(`buscando id de tabla: ${tabla} con los datos => campo: ${campo}, valor: ${valor} `.blue)
         const [result] = await pool.query(`SELECT id FROM ${tabla} WHERE ${campo} = '${valor}'`);
-        console.log(`id de ${tabla} obtenido: ${result.id}`)
+        console.log(`id de ${tabla} obtenido: ${result.id}`.blue)
         return result.id;
     } catch (error) {
         throw error;
@@ -44,9 +44,9 @@ const obtenerId = async (campo, valor, tabla) => {
 const mysqlSelect = async (table, fields, where, extraParams) => {
     try {
         const selectQuery = `SELECT ${fields} FROM ${table} ${where} ${extraParams}`;
-        console.log(`Realizando consulta => ${selectQuery} `)
+        console.log(`Realizando consulta => ${selectQuery} `.blue)
         const [result] = await pool.query(selectQuery);
-        console.log(`Consulta realizada, resultado: ${JSON.stringify(result)}`)
+        console.log(`Consulta realizada, resultado: ${JSON.stringify(result)}`.blue)
         return result;
     } catch (error) {
         throw error;
@@ -56,9 +56,9 @@ const mysqlSelect = async (table, fields, where, extraParams) => {
 const mySQLInsert = async (table, fields, values) => {
     try {
         const insertQuery = `INSERT INTO ${table} (${fields}) VALUES (${values})`;
-        console.log(`Realizando insercion => ${insertQuery} `)
+        console.log(`Realizando insercion => ${insertQuery} `.blue)
         const insertResult = await pool.query(insertQuery);
-        return `Insercion a tabla: ${table}, id: ${insertResult.insertId}`;
+        return `Insercion a tabla: ${table}, id: ${insertResult.insertId}`.blue;
     } catch (error) {
         throw error;
     }
@@ -76,7 +76,7 @@ const processPDFDataIntoBD = async (pdfObject) => {
         insertFields = `numero_expediente, folio, juez, actor, demandado, secretario, juicio_id, created_at, updated_at`;
         insertValues = `'${pdfObject.datosGenerales.numeroExpediente}', '${pdfObject.datosGenerales.folio}', '${pdfObject.datosGenerales.juez}', '${pdfObject.datosGenerales.parteActora}', '${pdfObject.datosGenerales.parteDemandada}', '${pdfObject.datosGenerales.secretario}', '${tipoJuicio}', NOW(), NOW()`
         const insercionExpediente = await mySQLInsert(`expedientes`, insertFields, insertValues);
-        console.log(insercionExpediente);
+        console.log(insercionExpediente.blue);
 
         // Inserting tokens
         const expediente = await mysqlSelect(`expedientes`, `id`, ``, `order by id desc limit 1`);
@@ -84,11 +84,11 @@ const processPDFDataIntoBD = async (pdfObject) => {
 
         insertValues = `'${pdfObject.tokenInvitado}', '${expediente.id}', NOW(), NOW()`
         const insercionTokenInvitado = await mySQLInsert(`token_audiencia_invitados`, insertFields, insertValues);
-        console.log(insercionTokenInvitado);
+        console.log(insercionTokenInvitado.blue);
 
         insertValues = `'${pdfObject.tokenAcceso}', '${expediente.id}', NOW(), NOW()`
         const insercionTokenAcceso = await mySQLInsert(`token_audiencias`, insertFields, insertValues);
-        console.log(insercionTokenAcceso);
+        console.log(insercionTokenAcceso.blue);
 
         // Inserting audiencias
 
@@ -101,7 +101,7 @@ const processPDFDataIntoBD = async (pdfObject) => {
         insertFields = `centroJusticia_id, sala_id, tipo_id, expediente_id, estadoAudiencia_id, fechaCelebracion, horaInicio, horaFinalizar, created_at, updated_at`;
         insertValues = `'${centroDeJusticiaId}', '${salaId}', '${tipoAudienciaId}', '${expediente.id}', '1', '${pdfObject.datosGenerales.fechaCelebracion}', '${pdfObject.datosGenerales.horaInicio}', '${pdfObject.datosGenerales.horaAFinalizar}', NOW(), NOW()`
         const insercionAudiencia = await mySQLInsert(`audiencias`, insertFields, insertValues);
-        console.log(insercionAudiencia);
+        console.log(insercionAudiencia.blue);
 
         // Inserting personal
 
@@ -112,29 +112,31 @@ const processPDFDataIntoBD = async (pdfObject) => {
         const juezRoleId = await obtenerId('tipo_personal', 'Juez', 'roles_personals');
         insertValues = `'${pdfObject.datosGenerales.juez}', '${juezRoleId}', '${audiencia.id}', NOW(), NOW()`
         insercionPersonal = await mySQLInsert(`personal_audiencias`, insertFields, insertValues);
-        console.log(insercionPersonal);
+        console.log(insercionPersonal.blue);
 
         const secretarioRoleId = await obtenerId('tipo_personal', 'Secretario', 'roles_personals');
         insertValues = `'${pdfObject.datosGenerales.secretario}', '${secretarioRoleId}', '${audiencia.id}', NOW(), NOW()`
         insercionPersonal = await mySQLInsert(`personal_audiencias`, insertFields, insertValues);
-        console.log(insercionPersonal);
+        console.log(insercionPersonal.blue);
 
-        const testigoRoleId = await obtenerId('tipo_personal', 'Testigo', 'roles_personals');
-        insertValues = `'${pdfObject.datosGenerales.testigo}', '${testigoRoleId}', '${audiencia.id}', NOW(), NOW()`
-        insercionPersonal = await mySQLInsert(`personal_audiencias`, insertFields, insertValues);
-        console.log(insercionPersonal);
+        if (pdfObject.datosGenerales.testigo){
+            const testigoRoleId = await obtenerId('tipo_personal', 'Testigo', 'roles_personals');
+            insertValues = `'${pdfObject.datosGenerales.testigo}', '${testigoRoleId}', '${audiencia.id}', NOW(), NOW()`
+            insercionPersonal = await mySQLInsert(`personal_audiencias`, insertFields, insertValues);
+            console.log(insercionPersonal.blue);
+        }
 
         const parteActoraRoleId = await obtenerId('tipo_personal', 'Parte Actora', 'roles_personals');
         insertValues = `'${pdfObject.datosGenerales.parteActora}', '${parteActoraRoleId}', '${audiencia.id}', NOW(), NOW()`
         insercionPersonal = await mySQLInsert(`personal_audiencias`, insertFields, insertValues);
-        console.log(insercionPersonal);
+        console.log(insercionPersonal.blue);
 
         const parteDemandadaRoleId = await obtenerId('tipo_personal', 'Parte Demandada', 'roles_personals');
         insertValues = `'${pdfObject.datosGenerales.parteDemandada}', '${parteDemandadaRoleId}', '${audiencia.id}', NOW(), NOW()`
         insercionPersonal = await mySQLInsert(`personal_audiencias`, insertFields, insertValues);
-        console.log(insercionPersonal);
+        console.log(insercionPersonal.blue);
 
-        return `PDF procesado a base de datos de forma exitosa :)`;
+        return `PDF procesado a base de datos de forma exitosa :)`.green;
 
     } catch (e) {
         throw e;
